70ed4acaf6cdda9f6e1704518573dbda63dc28fa
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-02-28 08:50:08 +0100

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            "white": "hsl(0, 0%, 100%)",
+            "light-gray": "hsl(212, 45%, 89%)",
+            "grayish-blue": "hsl(220, 15%, 55%)",
+            "dark-blue": "hsl(218, 44%, 22%)"
+        },
+        fontFamily: {
+            "body": ["Outfit", "sans-serif"],
+        },
+        screens: {
+            "sm": "375px",
+            "lg": "1440px"
+        }
+    }
+};
