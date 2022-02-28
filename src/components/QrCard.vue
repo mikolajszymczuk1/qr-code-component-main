f2ab02f024d091b4e7d866363aa25c8c794e891b
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-02-28 11:11:27 +0100

    Create QrCard component and add component's structure

---
+++ src/components/QrCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- Qr Card component -->
+    <div class="">
+        <img class="" src="@/assets/images/image-qr-code.png" alt="Qr Code image" title="Qr Code">
+        <h1 class="">Improve your front-end skills by building projects</h1>
+        <p class="">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "QrCard"
+}
+</script>
