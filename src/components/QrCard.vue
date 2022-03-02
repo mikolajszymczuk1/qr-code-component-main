6d9f814ea81c43b91b23103a43974f7afa78bd65
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-03-02 12:33:44 +0100

    Add styles to card component

--- src/components/QrCard.vue
+++ src/components/QrCard.vue
@@ -1,9 +1,9 @@
 <template>
     <!-- Qr Card component -->
-    <div class="">
-        <img class="" src="@/assets/images/image-qr-code.png" alt="Qr Code image" title="Qr Code">
-        <h1 class="">Improve your front-end skills by building projects</h1>
-        <p class="">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    <div class="bg-white font-body text-center sm:mx-auto sm:my-22 sm:p-4 sm:w-80 sm:rounded-2xl lg:mt-38">
+        <img class="mb-6 sm:rounded-lg" src="@/assets/images/image-qr-code.png" alt="Qr Code image" title="Qr Code">
+        <h1 class="mb-4 px-8 text-dark-blue text-heading font-bold leading-7 sm:p-0">Improve your front-end skills by building projects</h1>
+        <p class="px-8 pb-10 text-paragraph text-grayish-blue leading-5 sm:px-5 sm:pb-5">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
 </template>
 
